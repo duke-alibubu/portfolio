cffd414609af75e253b0481c728b9e1ff53c6526
duke.alibubu <ttdungalibubu@gmail.com>
2022-05-09 23:49:39 +0800

    skills

--- components/Sections/Skills.section.tsx
+++ components/Sections/Skills.section.tsx
@@ -3,45 +3,29 @@ import { BsArrowRightShort } from "../Misc/Icons.collection";
 const Skills = () => {
   return (
     <div className="my-16 px-3 font-sen text-white" id="skills">
-      <p className="text-3xl font-bold text-white">Skills & Uses</p>
+      <p className="text-3xl font-bold text-white">Skills</p>
 
       <div className="text-md my-8 flex flex-col font-medium md:text-xl custom:text-lg">
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">NextJS</span>
-          &nbsp;as my Frontend Framework
+          <span className="text-white">Programming Languages: Golang, Java, JavaScript, Python, C/C++, Kotlin, Ruby</span>
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">Typescript </span>
-          &nbsp;as my main language
+          <span className="text-white">Back-end Development</span>
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">Tailwind CSS </span>
-          &nbsp;as my CSS framework
+          <span className="text-white">Front-end Development</span>
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">Figma </span>
-          &nbsp;as my designing tool
-        </p>
-
-        <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
-          <BsArrowRightShort size="30" />
-          <span className="text-white">Express JS </span>
-          &nbsp;as my API framework
+          <span className="text-white">Mobile Application Development</span>
         </p>
       </div>
-
-      <p className="text-lg font-medium text-slate-300">
-        ...more skills include <span className="text-white">javascript</span>,{" "}
-        <span className="text-white">reactjs</span>,{" "}
-        <span className="text-white">fastapi </span>{" "}
-      </p>
     </div>
   );
 };
